cd7ee214a4226bf15f934176573ae1087a81fc82
robmolloy <robmolloy@hotmail.co.uk>
2025-11-09 21:19:10 +0000

    working

--- src/pages/CustomPageComponent.tsx
+++ src/pages/CustomPageComponent.tsx
@@ -8,44 +8,34 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url
 ).toString();
 
-const usePdfPageDimensions = (p: { initScale: number }) => {
-  const [scale, setScale] = useState<number>(p.initScale);
-  const [unscaledPageHeight, setUnscaledPageHeight] = useState<number>(0);
-
-  return {
-    scale,
-    setScale,
-    unscaledPageHeight,
-    setUnscaledPageHeight,
-  };
-};
-
-const CustomPdfPage = (p: {
-  pageNumber: number;
-  scale: number;
-  unscaledPageHeight: number;
-  pagesFromBottom: number;
-  onMousePositionChange: (p: { x: number; y: number } | null) => void;
-}) => {
+const CustomPdfPage = (p: { pageNumber: number; scale: number }) => {
+  const [mousePos, setMousePos] = useState<{ x: number; y: number } | null>(
+    null
+  );
+  const [unscaledPageHeight, setUnscaledPageHeight] = useState(0);
   const requestAnimationFrameRef = useRef<number | null>(null);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (requestAnimationFrameRef.current) return;
 
     requestAnimationFrameRef.current = requestAnimationFrame(() => {
-      const target = e.target as EventTarget & HTMLDivElement;
+      const target = e.target as HTMLDivElement;
       const rect = target.getBoundingClientRect();
 
-      const screenX = e.clientX - rect.left;
-      const screenY = e.clientY - rect.top;
+      const screenX = e.clientX;
+      const screenY = e.clientY;
 
-      const x = screenX / p.scale;
-      const y = p.unscaledPageHeight - screenY / p.scale;
+      const targetWidth = rect.left;
+      const targetHeight = rect.top;
 
-      const adjustedY = y + p.pagesFromBottom * p.unscaledPageHeight;
+      const targetX = screenX - targetWidth;
+      const targetY = screenY - targetHeight;
 
-      p.onMousePositionChange({ x, y: adjustedY });
+      const x = targetX / p.scale;
+      const y = unscaledPageHeight - targetY / p.scale;
+
       requestAnimationFrameRef.current = null;
+      setMousePos({ x, y });
     });
   };
 
@@ -58,12 +48,18 @@ const CustomPdfPage = (p: {
 
   return (
     <div>
+      {mousePos
+        ? `x: ${mousePos.x.toFixed(2)}, y: ${mousePos.y.toFixed(2)}`
+        : "N/A"}
       <Page
         pageNumber={p.pageNumber}
         scale={p.scale}
         onMouseMove={handleMouseMove}
-        onMouseLeave={() => p.onMousePositionChange(null)}
-        // onLoadSuccess={(page) => {}}
+        onMouseLeave={() => setMousePos(null)}
+        onLoadSuccess={(page) => {
+          const viewport = page.getViewport({ scale: 1 });
+          setUnscaledPageHeight(viewport.height);
+        }}
       />
     </div>
   );
@@ -71,66 +67,23 @@ const CustomPdfPage = (p: {
 
 export const CustomPageComponent = () => {
   const [numPages, setNumPages] = useState<number>();
-  const { scale, setScale, unscaledPageHeight } = usePdfPageDimensions({
-    initScale: 1,
-  });
-
-  const [mousePos, setMousePos] = useState<{ x: number; y: number } | null>(
-    null
-  );
+  const [scale, setScale] = useState<number>(1);
 
   return (
     <div>
-      <br />
-      <br />
       <button onClick={() => setScale((scale) => (scale += 0.25))}>++++</button>
       <button onClick={() => setScale((scale) => (scale -= 0.25))}>----</button>
       <button onClick={() => setScale(1)}>Reset</button>
-      <br />
       Scale: {scale}
       <br />
-      Mouse:{" "}
-      {mousePos
-        ? `x: ${mousePos.x.toFixed(2)}, y: ${mousePos.y.toFixed(2)}`
-        : "N/A"}
-      <br />
-      <br />
-      <div style={{ position: "relative" }}>
-        <Document
-          file="http://localhost:5173/lots-of-pages-of-links.pdf"
-          onLoadSuccess={(x) => setNumPages(x.numPages)}
-        >
-          {[...Array(numPages)].map((_, j) => (
-            <React.Fragment key={j}>
-              <CustomPdfPage
-                pageNumber={j + 1}
-                scale={scale}
-                unscaledPageHeight={unscaledPageHeight}
-                pagesFromBottom={(numPages ?? 0) - 1 - j}
-                onMousePositionChange={(coords) => setMousePos(coords)}
-              />
-              <br />
-            </React.Fragment>
-          ))}
-        </Document>
-
-        {mousePos && (
-          <div
-            style={{
-              position: "absolute",
-              left: `${mousePos.x * scale}px`,
-              bottom: `${mousePos.y * scale}px`,
-              width: "10px",
-              height: "10px",
-              background: "blue",
-              borderRadius: "50%",
-              pointerEvents: "none",
-              transform: "translate(-50%, 50%)",
-              opacity: 0.5,
-            }}
-          />
-        )}
-      </div>
+      <Document
+        file="http://localhost:5173/lots-of-pages-of-links.pdf"
+        onLoadSuccess={(x) => setNumPages(x.numPages)}
+      >
+        {[...Array(numPages)].map((_, j) => (
+          <CustomPdfPage key={j} pageNumber={j + 1} scale={scale} />
+        ))}
+      </Document>
     </div>
   );
 };

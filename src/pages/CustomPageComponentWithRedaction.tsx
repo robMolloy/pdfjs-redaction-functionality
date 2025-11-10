52ae25a88b0cbbde583ad0d2bc5fc6aabef842da
robmolloy <robmolloy@hotmail.co.uk>
2025-11-10 17:46:05 +0000

    improve

--- src/pages/CustomPageComponentWithRedaction.tsx
+++ src/pages/CustomPageComponentWithRedaction.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
@@ -8,25 +8,86 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url
 ).toString();
 
+type TCoord = { x: number; y: number };
+type TCoordPair = { x1: number; y1: number; x2: number; y2: number };
+
+const convertCoordPairToXywh = (p: {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}) => {
+  const xLeft = Math.min(p.x1, p.x2);
+  const xRight = Math.max(p.x1, p.x2);
+  const yBottom = Math.min(p.y1, p.y2);
+  const yTop = Math.max(p.y1, p.y2);
+
+  const width = xRight - xLeft;
+  const height = yTop - yBottom;
+
+  return { xLeft, yBottom, width, height };
+};
+
+type TMode = "textRedact" | "geometryRedact";
+
+const UninteractiveElementsStyleTag = () => (
+  <style>
+    {`
+        .react-pdf__Page__textContent a {
+          pointer-events: none !important;
+        }
+        .react-pdf__Page__textContent section {
+          pointer-events: none !important;
+        }
+      `}
+  </style>
+);
+
+const getPdfCoords = (p: {
+  screenX: number;
+  screenY: number;
+  scale: number;
+  targetRect: DOMRect;
+}) => {
+  const targetLeft = p.targetRect.left;
+  const targetTop = p.targetRect.top;
+  const targetBottom = p.targetRect.bottom;
+  const targetHeight = targetBottom - targetTop;
+
+  const targetX = screenX - targetLeft;
+  const targetY = screenY - targetTop;
+
+  const x = targetX / p.scale;
+  const y = (targetHeight - targetY) / p.scale; // required for bottom-left origin
+
+  return { x, y };
+};
+
 const CustomPdfPage = (p: {
   onMouseMove: (p: { x: number; y: number } | null) => void;
   pageNumber: number;
   scale: number;
+  mode: TMode;
 }) => {
   const [mousePos, setMousePos] = useState<{ x: number; y: number } | null>(
     null
   );
-
   useEffect(() => p.onMouseMove(mousePos), [mousePos]);
+
+  const [firstCorner, setFirstCorner] = useState<TCoord | null>(null);
+  const [redactions, setRedactions] = useState<TCoordPair[]>([]);
+
+  const pageWrapperElmRef = useRef<HTMLDivElement | null>(null);
+
   const requestAnimationFrameRef = useRef<number | null>(null);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (requestAnimationFrameRef.current) return;
 
     const target = e.target as HTMLDivElement;
-    if (!target.className.includes("react-pdf__Page__textContent"))
-      return setMousePos(null);
-
+    if (!target.className.includes("react-pdf__Page__textContent")) {
+      return;
+    }
     requestAnimationFrameRef.current = requestAnimationFrame(() => {
       const rect = target.getBoundingClientRect();
 
@@ -58,16 +119,108 @@ const CustomPdfPage = (p: {
 
   return (
     <div>
+      {p.mode === "geometryRedact" && <UninteractiveElementsStyleTag />}
+      <button
+        onClick={() => {
+          const selection = window.getSelection();
+          if (!selection) return;
+          const range = selection.getRangeAt(0);
+          const rects = range.getClientRects();
+          [...rects].forEach((r) => {
+            if (r.width < 3) return;
+            console.log(`CustomPageComponentWithRedaction.tsx:${/*LL*/ 109}`, {
+              r,
+            });
+          });
+        }}
+      >
+        Redact selected text
+      </button>
+      <button
+        onClick={() => {
+          console.log(`CustomPageComponentWithRedaction.tsx:${/*LL*/ 140}`, {
+            pageWrapperElmRef,
+            x: pageWrapperElmRef.current?.getBoundingClientRect(),
+          });
+        }}
+      >
+        log page
+      </button>
+      <pre>{JSON.stringify({ redactions })}</pre>
+      <br />
+      <pre>{JSON.stringify({ firstCorner })}</pre>
+      <br />
       {mousePos
         ? `x: ${mousePos.x.toFixed(2)}, y: ${mousePos.y.toFixed(2)}`
         : "N/A"}
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <Page
-          pageNumber={p.pageNumber}
-          scale={p.scale}
-          onMouseMove={handleMouseMove}
-          onMouseLeave={() => setMousePos(null)}
-        />
+        <div ref={pageWrapperElmRef} style={{ position: "relative" }}>
+          <Page
+            pageNumber={p.pageNumber}
+            onClick={() => {
+              if (p.mode === "textRedact") return;
+              if (firstCorner && mousePos) {
+                const newRect: TCoordPair = {
+                  x1: firstCorner.x,
+                  y1: firstCorner.y,
+                  x2: mousePos.x,
+                  y2: mousePos.y,
+                };
+                setRedactions((redactions) => [...redactions, newRect]);
+              }
+              setFirstCorner(firstCorner ? null : mousePos);
+            }}
+            scale={p.scale}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={() => setMousePos(null)}
+          />
+          {firstCorner &&
+            mousePos &&
+            (() => {
+              const { xLeft, yBottom, width, height } = convertCoordPairToXywh({
+                x1: firstCorner.x,
+                y1: firstCorner.y,
+                x2: mousePos.x,
+                y2: mousePos.y,
+              });
+
+              return (
+                <div
+                  style={{
+                    position: "absolute",
+                    left: `${xLeft * p.scale}px`,
+                    bottom: `${yBottom * p.scale}px`,
+                    width: `${width * p.scale}px`,
+                    height: `${height * p.scale}px`,
+                    background: "rgba(0, 0, 255, 0.2)",
+                    border: "2px dashed blue",
+                    pointerEvents: "none",
+                  }}
+                />
+              );
+            })()}
+
+          {redactions.map((box, i) => {
+            const { xLeft, yBottom, width, height } =
+              convertCoordPairToXywh(box);
+
+            return (
+              <div
+                key={i}
+                style={{
+                  position: "absolute",
+                  left: `${xLeft * p.scale}px`,
+                  bottom: `${yBottom * p.scale}px`,
+                  width: `${width * p.scale}px`,
+                  height: `${height * p.scale}px`,
+                  background: "rgba(255, 0, 0, 0.3)",
+                  border: "2px solid red",
+                  pointerEvents: "none",
+                }}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
@@ -82,8 +235,21 @@ export const CustomPageComponentWithRedaction = () => {
     y: number;
   } | null>(null);
 
+  const [mode, setMode] = useState<TMode>("textRedact");
+
   return (
     <div>
+      <button
+        onClick={() =>
+          setMode((x) =>
+            x === "geometryRedact" ? "textRedact" : "geometryRedact"
+          )
+        }
+      >
+        Redaction mode
+      </button>
+      ({mode})
+      <br />
       <button onClick={() => setScale((scale) => (scale += 0.25))}>++++</button>
       <button onClick={() => setScale((scale) => (scale -= 0.25))}>----</button>
       <button onClick={() => setScale(1)}>Reset</button>
@@ -92,7 +258,7 @@ export const CustomPageComponentWithRedaction = () => {
       mousePos: {JSON.stringify({ mousePos })}
       <br />
       <Document
-        file="http://localhost:5173/different-orientations-and-sizes.pdf"
+        file="http://localhost:5173/may-plus-images.pdf"
         onLoadSuccess={(x) => setNumPages(x.numPages)}
       >
         {[...Array(numPages)].map((_, j) => (
@@ -100,9 +266,10 @@ export const CustomPageComponentWithRedaction = () => {
             key={j}
             pageNumber={j + 1}
             scale={scale}
-            onMouseMove={(coords) =>
-              setMousePos(coords ? { pageIndex: j + 1, ...coords } : null)
-            }
+            onMouseMove={(coords) => {
+              setMousePos(coords ? { pageIndex: j + 1, ...coords } : null);
+            }}
+            mode={mode}
           />
         ))}
       </Document>

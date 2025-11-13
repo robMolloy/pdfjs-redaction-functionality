b335815da84e3772a6709feb2da918c9ddb3db2f
robmolloy <robmolloy@hotmail.co.uk>
2025-11-13 09:12:34 +0000

    test

--- src/packages/documentViewerAndRedactor/DocumentViewerAndRedactor.tsx
+++ src/packages/documentViewerAndRedactor/DocumentViewerAndRedactor.tsx
@@ -60,6 +60,7 @@ export const DocumentViewerAndRedactor = (p: { fileUrl: string }) => {
       <div
         style={{
           border: "1px solid black",
+
           background: "white",
           color: "black",
           display: "flex",
